87583fab7afd5ce3ed1c9031e8544859e19597f1
Pablo Estigarribia <pablolestigarribia@gmail.com>
2022-10-31 20:37:02 -0300

    Carrito completado

--- cart.js
+++ cart.js
@@ -1,12 +1,15 @@
 let carritoContainer = document.querySelector(".cart-products-container");
 let carritoVacio = document.querySelector(".empty-cart"); // Aviso de "¡No hay productos en el carrito!"
+let divisor = document.querySelector(".divisor");
+let total = document.querySelector(".total");
+let buyButton = document.querySelector(".buy-button");
 let carrito = [];
+let precioTotal = 0;
 
-function ProductoEnCarrito(id, name, price, quantity) {
+function ProductoEnCarrito(id, name, price) {
     this.id = id;
     this.name = name;
     this.price = price;
-    this.quantity = quantity;
 }
 
 // carrito.push(card.querySelector(".product-title").textContent);
@@ -27,7 +30,7 @@ function escucharClickEnProducto() {
                     let producto = new ProductoEnCarrito(
                         nft.id,
                         nft.name,
-                        nft.price, 1);
+                        nft.price);
 
                     let productoDuplicado = false;
                     carrito.forEach(producto => {
@@ -48,9 +51,8 @@ function escucharClickEnProducto() {
                         // });
                         // (Olvidé que los NFT no se pueden duplicar =_=💧)
                     } else {
-                        console.log(carrito);
                         carrito.push(producto);
-                        renderizarCarrito(producto.id, producto.name, producto.price, producto.quantity);
+                        renderizarCarrito(producto.id, producto.name, producto.price);
                     }
                 }
             })
@@ -58,27 +60,73 @@ function escucharClickEnProducto() {
     })
 }
 
-function renderizarCarrito(id, name, price, quantity) {
-    console.log('Carrito renderizado')
-    carritoVacio.classList.add("hidden");
-
+function renderizarCarrito(id, name, price) {
     const div = document.createElement("div");
-    div.classList.add("cart-product", id);
+    div.classList.add("cart-product", 'cart-product-id--' + id);
     carritoContainer.append(div);
     div.innerHTML = `<img src="./img/NFTs/${id}.png">
     <div class="product-text">
         <p class="product-title">DigiDaigaku #${id}</p>
         <p class="product-subtitle">${name}</p>
         
-        <div class="product-quantity">
+        <div class="product-priceanddelete">
             <div class="product-price">  
                 <p><i class="fa-brands fa-ethereum"></i>${price}</p>
             </div>
-            <i class="fa-solid fa-circle-minus"></i>
-            <p class="quantity">${quantity}</p>
-            <i class="fa-solid fa-circle-plus"></i>
+            <i class="product-delete fa-solid fa-xmark fa-lg" id="${id}"></i>
         </div>
     </div>`
+
+    actualizarPrecio()
 }
 
+function actualizarCarrito() {
+    if (carrito.length > 0) {
+        carritoVacio.classList.add("hidden");
+        divisor.classList.remove("hidden");
+        total.classList.remove("hidden");
+        buyButton.classList.remove("hidden");
+    } else {
+        carritoVacio.classList.remove("hidden");
+        divisor.classList.add("hidden");
+        total.classList.add("hidden");
+        buyButton.classList.add("hidden");
+    }
+}
 
+function actualizarPrecio() {
+    let totalElement = document.querySelector(".total-num");
+    let total = 0;
+    carrito.forEach(item => { total += item.price })
+    totalElement.innerHTML = `<i class="fa-brands fa-ethereum"></i><p class="price"> ${total}</p>`;
+}
+
+document.addEventListener('click', () => {
+    deleteProductHandler();
+    actualizarCarrito()
+})
+
+function deleteProductHandler() {
+    window.onclick = e => {
+        let targetId = e.target.id;
+        carrito.forEach(element => {
+            if (element.id == targetId) {
+                carrito = carrito.filter(function (item) { return item.id != targetId });
+                deleteProductFromHTML(targetId);
+            }
+        })
+    }
+}
+
+function deleteProductFromHTML(id) {
+    let selector = '.' + 'cart-product-id--' + id.toString();
+    const element = document.querySelector(selector);
+    element.remove();
+    actualizarPrecio();
+    actualizarCarrito()
+}
+
+buyButton.addEventListener('click', () => {
+    alert("COMPRA REALIZADA.\nLos NFT fueron transferidos a la wallet asociada a su cuenta.");
+    window.location.reload();
+})
